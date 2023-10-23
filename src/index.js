0e2179909312d43a5af00f1e8046a5e6b84a97fb
Dmitriy B <i@bekesh.ru>
2023-10-23 21:14:09 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -39,6 +39,39 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+
+    let result = '';
+
+
+    for (let i = 0; i < expr.length; i += 10) {
+        let simbol = expr.slice (i, i + 10);
+        //console.log(simbol);
+        if (simbol === '**********') {
+            result += ' ';
+            //console.log('space');
+        } else {
+            //console.log('xxx');
+            let morseCode = '';
+            for (let j = 0; j < simbol.length; j += 2) {
+            let code = simbol.slice (j, j + 2);
+                if (code === '10') {
+                    morseCode += '.';
+                    //console.log('to4ka', morseCode);
+                    
+                } else if (code === '11') {
+                    morseCode += '-';
+                    //console.log('tire', morseCode);
+                }
+    
+        }
+        result += MORSE_TABLE[morseCode];
+        //console.log('itog,', result);
+    }
+    }
+    return result;
+    
+    console.log(result);
+
 }
 
 module.exports = {
